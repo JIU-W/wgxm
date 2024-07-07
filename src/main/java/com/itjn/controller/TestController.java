42bf7372e35af3a12767191707bbf48935f50706
JIU-W <2661727280@qq.com>
2024-07-07 16:39:48 +0800

    测试接口

--- src/main/java/com/itjn/controller/TestController.java
+++ src/main/java/com/itjn/controller/TestController.java
@@ -1,11 +1,21 @@
 package com.itjn.controller;
 
 
+import com.itjn.common.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("")
 public class TestController {
+
+    @GetMapping("/test")
+    public Result test(){
+        log.info("测试成功");
+        return Result.success();
+    }
 
 }
